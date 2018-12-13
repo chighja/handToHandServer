29d84594e43316b24e262e6174ae4a5c864150f6
chighja <chighja@gmail.com>
2018-12-13 18:28:34 -0500

    corrected testDB url in config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ module.exports = {
     'mongodb://useradmin:Password1@ds155268.mlab.com:55268/hand-to-hand',
   TEST_DATABASE_URL:
     process.env.TEST_DATABASE_URL ||
-    'mongodb://useradmin:Password1@ds155288.mlab.com:55268/hand-to-hand-test',
+    'mongodb://useradmin:Password1@ds155288.mlab.com:55288/hand-to-hand-test',
   PORT: process.env.PORT || 'http://localhost:5000'
 };
