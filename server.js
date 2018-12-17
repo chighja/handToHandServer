9eaa21c70bc319ab9e1db3857c09c09e449fd441
chighja <chighja@gmail.com>
2018-12-17 17:44:16 -0500

    updated CORS method information

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(function(req, res, next) {
     'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept'
   );
-  res.header('AccessControlAllowMethods', 'GET, PATCH');
+  res.header('Access-Control-Allow-Methods', 'GET, PATCH');
   next();
 });
 
