4cf36ad16212131c701e794f109cc5522214950e
C <44075957+chighja@users.noreply.github.com>
2019-02-16 09:53:39 -0500

    Updated client address

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   CLIENT_ORIGIN:
-    process.env.CLIENT_ORIGIN || 'https://warm-inlet-87726.herokuapp.com',
+    process.env.CLIENT_ORIGIN || 'https://handtohandapp.herokuapp.com',
   DATABASE_URL:
     process.env.DATABASE_URL ||
     'mongodb://useradmin:Password1@ds155268.mlab.com:55268/hand-to-hand',
